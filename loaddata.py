59e227090e7d1cf441b2489234d93ad397b7e2ee
aliaksandrkazlou <wec@tut.by>
2021-11-08 10:30:17 +0100

    С визуализацией

--- loaddata.py
+++ loaddata.py
@@ -5,13 +5,15 @@ from config import COLUMN_FOR_VIEW
 def save_file_for_view(file_name, file_to_save):
     df = pd.read_csv(file_name)
     df = df[COLUMN_FOR_VIEW]
-    df['DATE'] = pd.to_datetime(df['VREMEIZLAZ']).dt.date
-    df['TIME'] = pd.to_datetime(df['VREMEIZLAZ']).dt.hour
-    del(df['VREMEIZLAZ'])
-    df.columns = ['Номер машины', 'Участок', 'Цена проезда', 'TAGID', 'Дата', 'Время']
+    df['DATE'] = pd.to_datetime(df['VREMEIZLAZ'], format='%Y-%m-%d %H:%M:%S')
+    # df['TIME'] = df['DATE'].dt.hour
+
+    # df['TIME'] = pd.to_datetime(df['VREMEIZLAZ']).dt.hour
+    # del(df['VREMEIZLAZ'])
+    # df.columns = ['Номер машины', 'Участок', 'Цена проезда', 'TAGID', 'Дата', 'Время']
     print(df.info())
     print(df.head())
-    df.to_csv(file_to_save, index=False)
+    # df.to_csv(file_to_save, index=False)
 
 
 if __name__ == '__main__':
