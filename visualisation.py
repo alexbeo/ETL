5a85c68fcb31d8fc88635f8f3ed1f161f0b57024
aliaksandrkazlou <wec@tut.by>
2021-12-14 08:25:46 +0100

    С визуализацией

--- visualisation.py
+++ visualisation.py
@@ -1,28 +1,39 @@
+import datetime
+import os
+
 import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
 import plotly.express as px
-from config import PATH_TO_VIEW_FILE
+from config import PATH_TO_VIEW_FILE, PATH_TO_DAILY_REPORT_FILE_PS
 
 st.header("Анализ данных поездок по дорогам")
 
 
 @st.experimental_memo
 def load_data():
-    df = pd.read_csv(PATH_TO_VIEW_FILE)
-    return df
+    file_name = datetime.date.today().strftime('%d_%m_%y')+'.csv'
+    full_path_to_save = os.path.join(PATH_TO_DAILY_REPORT_FILE_PS, file_name)
+    try:
+        df = pd.read_csv(full_path_to_save)
+        return df
+    except Exception as ex:
+        print(f'возникла ошибка чтения файла {full_path_to_save}')
 
 
 data = load_data()
 st.header("Общее Количество Поездок за время")
+years = data.index.unique()
+year = st.sidebar.slider("Год", 2021, 2050)
 hour = st.sidebar.slider("Шкала времени", 0, 23)
-data = data[data['Время'] == hour]
+
+data = data[data['ЧАСЫ'] == hour]
 
 st.markdown("Проезды между %i:00 и %i:00 " %(hour, (hour+1)))
 
 filtered = data[
-           (data['Время'] >= hour) & (data['Время'] <= (hour+1))
+           (data['ЧАСЫ'] >= hour) & (data['ЧАСЫ'] <= (hour+1))
 ]
 
 # hist = np.histogram(pd.to_datetime(filtered['Время']).dt.minute, bins=60, range=(0, 60))[0]
